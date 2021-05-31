71d17de877ab0a9a3b70da0947d23f1ae76518ef
tangyao <1013654193@qq.com>
2021-06-01 01:30:51 +0800

    八皇后位运算解决方案

--- src/main/java/left/base/class07/Code09_NQueens.java
+++ src/main/java/left/base/class07/Code09_NQueens.java
@@ -46,10 +46,101 @@ public class Code09_NQueens {
     }
 
 
-
     public static void main(String[] args) {
         int number = getNumber(8);
         System.out.println("number = " + number);
+
+        int number2 = getNumber2(8);
+        System.out.println("number2 = " + number2);
+
     }
 
+    public static int getNumber2(int n) {
+        if (n < 1 || n > 32) {
+            return 0;
+        }
+
+        // 假如 n == 8 limit 就为 0000 0000 0000 0000 0000 0000 1111 1111
+        //limit 作用是 限制位数
+        int limit = n == 32 ? -1 : (1 << n) - 1;
+        return process2(limit, 0, 0, 0);
+
+    }
+
+    /**
+     * 左中右限制
+     *
+     * @param limit       限制死可以在哪些位置尝试皇后
+     * @param colLim      之前皇后对我列的限制
+     * @param leftDiaLim  之前皇后对我左斜线的限制
+     * @param rightDialim 之前皇后对我右斜线的限制
+     * @return
+     */
+    private static int process2(int limit, int colLim, int leftDiaLim, int rightDialim) {
+
+        // base case
+        if (colLim == limit) {
+            return 1;
+        }
+
+        int mostRightOne = 0;
+        // & 两个都是1 才为 1 ，否则为 0
+        // | 两个数一个为1 就为 1 ，否则为 0 ，
+        // limit 是为了截掉 8皇后 前面所有的 0（不能填皇后的位置）
+        // 所有可以填皇后的列都在这个位信息上
+        int pos = limit & (~(colLim | leftDiaLim | rightDialim));
+
+        int res = 0;
+        while (pos != 0) {
+            // 提取出候选皇后位置状态中最右侧的1来
+            mostRightOne = pos & (~pos + 1);
+            pos = pos - mostRightOne;
+
+            res += process2(limit, colLim | mostRightOne,
+                    (leftDiaLim | mostRightOne) << 1,
+                    (rightDialim | mostRightOne) >>> 1);
+        }
+
+        return res;
+
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
