0ba4ffb08c70afbf99b84e3e31ff9c90ba62ea77
tangyao <1013654193@qq.com>
2021-06-08 01:41:36 +0800

    基础提升， 设计 RandomPool

---
+++ src/main/java/left/baseascension/Code02_RandomPool.java
@@ -0,0 +1,69 @@
+package left.baseascension;
+
+import java.util.HashMap;
+
+/**
+ * @author tangyao
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2021年06月08日 01:04:00
+ */
+public class Code02_RandomPool {
+
+    public static class Pool<K> {
+
+        HashMap<K, Integer> keyIndexMap;
+        HashMap<Integer, K> indexKeyMap;
+        int size;
+
+        public Pool() {
+            this.keyIndexMap = new HashMap<>();
+            this.indexKeyMap = new HashMap<>();
+            size = 0;
+        }
+
+        public void insert(K key) {
+
+            if (!keyIndexMap.containsKey(key)) {
+                keyIndexMap.put(key, size);
+                indexKeyMap.put(size++, key);
+            }
+
+        }
+
+        public void delete(K key) {
+            if (keyIndexMap.containsKey(key)) {
+                Integer deleteIndex = keyIndexMap.get(key);
+                Integer lastIndex = --size;
+                K lastKey = indexKeyMap.get(lastIndex);
+                keyIndexMap.put(lastKey, deleteIndex);
+                indexKeyMap.put(deleteIndex, lastKey);
+
+                indexKeyMap.remove(deleteIndex);
+                keyIndexMap.remove(key);
+
+            }
+        }
+
+        public K getRandom() {
+
+            return size == 0
+                    ? null
+                    : indexKeyMap.get((int) (Math.random() * size));
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Pool<String> pool = new Pool<String>();
+        pool.insert("zuo");
+        pool.insert("cheng");
+        pool.insert("yun");
+        System.out.println(pool.getRandom());
+        System.out.println(pool.getRandom());
+        System.out.println(pool.getRandom());
+        System.out.println(pool.getRandom());
+        System.out.println(pool.getRandom());
+        System.out.println(pool.getRandom());
+    }
+}
