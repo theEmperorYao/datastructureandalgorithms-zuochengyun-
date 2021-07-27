33e82b3e825ecf475139ca03e6496b9a614b63dc
tangyao <1013654193@qq.com>
2021-07-27 23:59:23 +0800

    用动态规划改 获得最好结果

--- src/main/java/left/base/class08/Code08_CardsInLine.java
+++ src/main/java/left/base/class08/Code08_CardsInLine.java
@@ -12,6 +12,7 @@ public class Code08_CardsInLine {
 
     /**
      * 第一次选，选择最好结果
+     *
      * @param arr
      * @param left
      * @param right
@@ -26,6 +27,7 @@ public class Code08_CardsInLine {
 
     /**
      * 第二次选，返回最差结果，剩下最好的结果给自己
+     *
      * @param arr
      * @param left
      * @param right
@@ -46,10 +48,35 @@ public class Code08_CardsInLine {
         return Math.max(f(arr, 0, arr.length - 1), s(arr, 0, arr.length - 1));
     }
 
+    public static int win2(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+
+        int[][] f = new int[arr.length][arr.length];
+        int[][] s = new int[arr.length][arr.length];
+
+        for (int j = 0; j < arr.length; j++) {
+
+            f[j][j] = arr[j];
+            for (int i = j - 1; i >= 0; i--) {
+                f[i][j] = Math.max(arr[i] + s[i + 1][j], arr[j] + s[i][j - 1]);
+                s[i][j] = Math.min(f[i + 1][j], f[i][j - 1]);
+            }
+        }
+
+
+        return Math.max(f[0][arr.length - 1], s[0][arr.length - 1]);
+    }
+
     public static void main(String[] args) {
-        int[] arr = {1, 19, 1};
+        int[] arr = {1, 19, 1, 33, 4, 5, 34, 41};
         int i = win1(arr);
         System.out.println("i = " + i);
+
+        int i1 = win2(arr);
+        System.out.println("i1 = " + i1);
+
     }
 
 }
