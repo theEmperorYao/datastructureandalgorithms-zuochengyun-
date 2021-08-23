0da47068dffe90d9bd6ff01ffa7a173c9e1ba513
tangyao <1013654193@qq.com>
2021-08-23 15:17:49 +0800

    需改并查集

--- src/main/java/left/baseascension/code2/Code04_UnionFind.java
+++ src/main/java/left/baseascension/code2/Code04_UnionFind.java
@@ -1,5 +1,7 @@
 package left.baseascension.code2;
 
+import left.baseCopy.Code_32_UnionFind;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -28,75 +30,65 @@ public class Code04_UnionFind {
         //2.记录自己的父节点
         //3.如果自己就是父节点，记录自己的子节点个数
         private HashMap<V, Element<V>> elementHashMap;
-        private HashMap<Element<V>, Element<V>> fatherMap;
+        private HashMap<Element<V>, Element<V>> fatherHashMap;
         private HashMap<Element<V>, Integer> rankMap;
 
         public UnionFindSet(List<V> list) {
             this.elementHashMap = new HashMap<>();
-            this.fatherMap = new HashMap<>();
+            this.fatherHashMap = new HashMap<>();
             this.rankMap = new HashMap<>();
 
             for (V item : list) {
                 Element<V> element = new Element<V>(item);
                 elementHashMap.put(item, element);
-                fatherMap.put(element, element);
+                fatherHashMap.put(element, element);
                 rankMap.put(element, 1);
             }
 
         }
 
 
-        public boolean isSameSet(V a, V b) {
+        public boolean isSameSet(V v1, V v2) {
 
-            if (!elementHashMap.containsKey(a) || !elementHashMap.containsKey(b)) {
+            if (!elementHashMap.containsKey(v1) || !elementHashMap.containsKey(v2)) {
                 return false;
             }
-
-            return findFather(elementHashMap.get(a)) == findFather(elementHashMap.get(b));
+            return findFather(elementHashMap.get(v1)) == findFather(elementHashMap.get(v2));
         }
 
-        private Element<V> findFather(Element<V> element) {
-            Stack<Element<V>> stack = new Stack<>();
+        public void union(V v1, V v2) {
+            if (!isSameSet(v1, v2)) {
+                Element<V> e1 = findFather(elementHashMap.get(v1));
+                Element<V> e2 = findFather(elementHashMap.get(v2));
 
-            while (element != fatherMap.get(element)) {
-                stack.push(element);
-                element = fatherMap.get(element);
+                Integer r1 = rankMap.get(e1);
+                Integer r2 = rankMap.get(e2);
+
+                Element<V> big = r1 > r2 ? e1 : e2;
+                Element<V> small = big == e1 ? e2 : e1;
+
+                fatherHashMap.put(small, big);
+                Integer smallRank = rankMap.get(small);
+                rankMap.put(big, rankMap.get(big) + smallRank);
+                rankMap.remove(small);
             }
-            for (Element<V> item : stack) {
-                fatherMap.put(item, element);
+
+        }
+
+        public Element<V> findFather(Element<V> element) {
+
+            Stack<Element> stack = new Stack<>();
+            while (element != fatherHashMap.get(element)) {
+                stack.push(element);
+                element = fatherHashMap.get(element);
+            }
+
+            for (Element ele : stack) {
+                fatherHashMap.put(ele, element);
                 rankMap.put(element, rankMap.get(element) + 1);
             }
 
             return element;
-        }
-
-        public void union(V a, V b) {
-            if (isSameSet(a, b)) {
-                return;
-            }
-            Element<V> aFather = findFather(elementHashMap.get(a));
-            Element<V> bFather = findFather(elementHashMap.get(b));
-//            Element<V> big = rankMap.get(aFather) >= rankMap.get(bFather) ? aFather : bFather;
-//
-//            if (big == aFather) {
-//                fatherMap.put(bFather, aFather);
-//                rankMap.put(aFather, rankMap.get(aFather) + rankMap.get(bFather));
-//                rankMap.remove(bFather);
-//            } else {
-//                fatherMap.put(aFather, bFather);
-//                rankMap.put(bFather, rankMap.get(bFather) + rankMap.get(aFather));
-//                rankMap.remove(aFather);
-//            }
-
-            if (aFather != bFather) {
-                Element<V> big = rankMap.get(aFather) >= rankMap.get(bFather) ? aFather : bFather;
-                Element<V> small = big == aFather ? bFather : aFather;
-                fatherMap.put(big, small);
-                rankMap.put(big, rankMap.get(big) + rankMap.get(small));
-                rankMap.remove(rankMap.get(small));
-            }
-
-
         }
 
     }
@@ -110,6 +102,14 @@ public class Code04_UnionFind {
         unionFindSet.union(1, 4);
         sameSet = unionFindSet.isSameSet(1, 4);
         System.out.println("sameSet = " + sameSet);
+
+        unionFindSet.union(5,3);
+        unionFindSet.union(3,6);
+        boolean sameSet1 = unionFindSet.isSameSet(3, 4);
+        System.out.println("sameSet1 = " + sameSet1);
+        unionFindSet.union(3,4);
+        boolean sameSet2 = unionFindSet.isSameSet(3, 4);
+        System.out.println("sameSet2 = " + sameSet2);
 
 
     }
