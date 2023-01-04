ffd54edb505627a8cc7cb27f756d31ae0a83e4d2
tangyao <tangyao@shuzijz.cn>
2023-01-04 10:46:14 +0800

    完善4的倍数

--- src/main/java/left/intermediate/class05/NearMultiple4Times.java
+++ src/main/java/left/intermediate/class05/NearMultiple4Times.java
@@ -1,5 +1,7 @@
 package left.intermediate.class05;
 
+import java.lang.reflect.Array;
+
 /**
  * @Classname NearMultiple4Times
  * @Description
@@ -16,11 +18,28 @@ public class NearMultiple4Times {
     public static boolean nearMultiple4Times(int[] nums) {
 
         // 统计 奇数个数 a，2因子个数 b ，4因子个数 c
-        // 如果 a==0  ; 奇4奇4奇4奇   c>=b-1
-        // 如果 a!=0  如果a==1 false 否则 2222222 4奇4奇4奇    c>=b 如果
+        // 如果 a==0  ; 奇4奇4奇4奇   c>=a-1
+        // 如果 a!=0   否则 2222222 4奇4奇4奇  c>=a
+        int a = 0, b = 0, c = 0;
+        for (int num : nums) {
+            if (num % 2 == 1) {
+                a++;
+            }
+            if (num % 2 == 0 && num % 4 != 0) {
+                b++;
+            }
+            if (num % 4 == 0) {
+                c++;
+            }
 
-        return false;
+        }
+        return b == 0 ? c >= a - 1 : c >= a;
+    }
 
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 3, 33, 3, 4, 4, 4};
+        boolean b = nearMultiple4Times(arr);
+        System.out.println("b = " + b);
     }
 
 }
